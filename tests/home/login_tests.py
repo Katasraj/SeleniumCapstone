108ad8ca2f1a961c72d8e84bc47485ba72c23909
nagamurali-kotari <nagamurali_kotari@apple.com>
2024-12-06 11:07:05 +0530

    Allure report steps added to login_tests.py

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -31,10 +31,15 @@ class LoginTests(unittest.TestCase):
     @pytest.mark.run(order=2)
     @allure.step("Verifying with proper username/password credentials")
     def test_validLogin(self):
-        self.lp.clearItem(self.locator['emialLogin'],locatorType='xpath')
-        self.lp.login('kotarinaga1@gmail.com', 'Katasraj111#')
+        with allure.step("Clearing the login text field"):
+            self.lp.clearItem(self.locator['emialLogin'],locatorType='xpath')
+
+        with allure.step("Entering proper Username and Password"):
+            self.lp.login('kotarinaga1@gmail.com', 'Katasraj111#')
         result1 = self.lp.verifyLoginTitle()
-        self.ts.mark(result1,"Title Verified")
+
+        with allure.step("Verifying Title"):
+            self.ts.mark(result1,"Title Verified")
         result2 = self.lp.verifyLoginSuccessful()
         time.sleep(5)
         self.ts.markFinal("test_ValidateLogin",result2,"Login Verification")
@@ -43,6 +48,8 @@ class LoginTests(unittest.TestCase):
     @allure.step("Verifying with wrong credentials")
     def test_invalidLogin(self):
         self.lp.logout()
-        self.lp.login('kotarinaga1@gmail.com','Katasraj123#')
+
+        with allure.step("Entering wrong password"):
+            self.lp.login('kotarinaga1@gmail.com','Katasraj123#')
         result = self.lp.verifyLoginFailed()
         assert result == True
